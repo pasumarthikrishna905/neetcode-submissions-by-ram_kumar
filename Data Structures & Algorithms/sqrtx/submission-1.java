595b34204f5d3beb35a8da69b4047a3ba3d31a26
Ramkumar <pasumarthikrishna905@gmail.com>
2026-08-20 17:38:50 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mySqrt(int x)
+    {
+        if(x<2)
+        {
+           return x;
+        }
+        int low=1;
+        int high=x/2;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if((long)mid*mid==x)
+            {
+                return mid;
+            }
+            else if((long)mid*mid<x)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+
+        }
+        return high;
+    }
+}
